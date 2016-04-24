89a7e178d0685a754cabb6ae8f4c83d6f0e4dbf7
Dquarick <davequarick@gmail.com>
2016-04-24 19:22:19 -0400

    made changes to char encoding for packets

--- python_networking/tcpclient.py
+++ python_networking/tcpclient.py
@@ -2,16 +2,16 @@ import socket
 
 
 def main():
-    host = '192.168.0.1'
-    port = 5000
+    host = '127.0.0.1'  # pointer address for host if on same machine as client
+    port = 5000  # the port on the host machine to which our request will be sent
 
     requesting_socket = socket.socket()
     requesting_socket.connect((host, port))
 
     message = input("->")
     while message != 'q':
-        requesting_socket.send(message)
-        data = requesting_socket.recv(1024)
+        requesting_socket.send(bytes(message, 'utf-8'))  # specifies char encoding
+        data = requesting_socket.recv(1024)  # size of the buffer
         print('Received from server: ' + str(data))
         message = input("->")
     requesting_socket.close()
--- python_networking/tcphost.py
+++ python_networking/tcphost.py
@@ -1,23 +1,25 @@
+# A basic iterative server script in Python 3.5 that returns client strings
+# in UPPERCASE
 import socket
 
 
 def main():
-    host = '192.168.0.8'  # the IP of the server
-    port = 5000  # the port in which the client must connect
+    host = '127.0.0.1'  # the local IP of the server (this one is for vir testing)
+    port = 5000  # the port in which the client must send requests and connect
 
     receiving_socket = socket.socket()
-    receiving_socket.bind((host, port))
+    receiving_socket.bind((port, host))  # linking machines IP,port,and socket
 
-    receiving_socket.listen(1)  # connections at a time - use queue > 1
+    receiving_socket.listen(1)  # connections at a time - if n_con > 1: use queue
     cur_connection, sender_address = receiving_socket.accept()
     print('Connection from:' + str(sender_address))
     while True:
-        data = cur_connection.recv(1024)  # buffer size in bits
+        data = cur_connection.recv(1024)  # buffer size
         if not data:
             break
         print('From connected user: ' + str(data))
         data = str(data).upper()
-        print('...sending: ' + str(data))
+        print('Returned to client: ' + str(data))
         cur_connection.send(bytes(data, 'utf-8'))
     cur_connection.close()
 if __name__ == '__main__':
