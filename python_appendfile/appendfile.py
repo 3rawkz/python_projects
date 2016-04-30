e22d9dc3e3c8a7437320f88ccda5222d59982d16
Dquarick <davequarick@gmail.com>
2016-04-30 02:32:12 -0400

    Python Example: Appending a file

---
+++ python_appendfile/appendfile.py
@@ -0,0 +1,8 @@
+# appending new data to a prexisting file
+
+append_me = "Here's new information!\nSo exciting!"
+
+# the second argument represents the action we plan to perform to the file - 'a' = append
+save_file = open('c:/users/dave quarick/desktop/python_projects/python_writefile', 'a')
+save_file.write(append_me)
+save_file.close() 
