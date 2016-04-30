cc2de29442c6ffd44e86340fb2a975feeeee2624
Dquarick <davequarick@gmail.com>
2016-04-30 02:50:41 -0400

    Example: Appending text to the end of a file

--- python_appendfile/appendfile.py
+++ python_appendfile/appendfile.py
@@ -2,7 +2,7 @@
 
 append_me = "Here's new information!\nSo exciting!"
 
-# the second argument represents the action we plan to perform to the file - 'a' = append
+# the second argument represents the action we plan to perform to the file: 'a' = append
 save_file = open('c:/users/dave quarick/desktop/python_projects/python_writefile', 'a')
 save_file.write(append_me)
 save_file.close() 
