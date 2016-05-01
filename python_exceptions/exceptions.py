b391f3e1d3d365fe499e6d684f6e0a8af9886a48
Dquarick <davequarick@gmail.com>
2016-04-30 21:27:43 -0400

    Change the effing tabs in show()

--- python_exceptions/exceptions.py
+++ python_exceptions/exceptions.py
@@ -2,17 +2,23 @@
 # Adding pets to pound inventory and keeping PETA in business
 # TODO: PUT THESE CLASSES IN SEPARATE FILES YOU BUM
 from string import Template
+# a little module that jazzes up console output
+from termcolor import*
+
 
 class PetTemplate(Template):
-	delimiter = '#'  # changing it because the standard is '$' and there's a price field
+	# changing it because the standard is '$' and there's a price field
+	delimiter = '#'
 	
 class Pet:
-	# note the elusive curly braces for dictionary def - this is a mutable structure
-	info = {'Name':"", 'Species':"", 'Age':0, 'Price':0}
-	
+	# globalClassVar = 0 ... no cool private, public kw's in Python! 
+
 	# default constructor
 	def __init__ (self):
 		try:
+			# note the elusive curly braces for dictionary def - this is a mutable structure
+			# also note that this is how we make structures and vars assigned to an instance (private)
+			self.info = {'Name':"", 'Species':"", 'Age':0, 'Price':0}
 			self.info['Name'] = input("Name: ")
 			self.info['Species'] = input("Species: ")
 			self.info['Age'] = int(input("Age: "))
@@ -28,40 +34,47 @@ class Inventory:
 	
 	# print current inventory
 	def show(self):
-		temp = PetTemplate("Name: #Name Species: #Species Age: #Age Price: $#Price")
+		temp = PetTemplate("Name: #Name \t\t Species: #Species \t\t Age: #Age \t\t Price: $#Price")
 		for pet in self.inventory:
-			print(temp.substitute(pet.info))
+			pet_info = temp.substitute(pet.info)
+			print(pet_info)
 	
 	# add a new pet to the inventory
 	def add(self):
 		self.inventory.append(Pet())
+	
+	# determining the total pets in inventory
+	def size(self):
+		print(len(self.inventory)) 
 
 def main():
 	inventory = Inventory()
+	user_dirs = "\nWhat would you like to do?\nEnter add, show, size or q to quit."
+	prompt = colored('-->', 'cyan')
 
-	print("What would you like to do?\nEnter add, show, or quit.")
-	user_in = input('-->')
-	
 	# this flow subs for a switch in other languages
-	while user_in != 'quit':
+	while 1 == 1:
+		print(user_dirs)
+		user_in = input(prompt)
+		
 		if user_in == 'add':
 			inventory.add()
-			print("Entry successfully added.\n________________________\n")
-			print("What would you like to do?\nEnter add, show, or quit.")
-			user_in = input('-->')
+			print(colored("SUCCESS!\n", 'green', attrs =['underline']))
 		
 		elif user_in == 'show':
-			print("Current Inventory:\n___________________")
+			print(colored("Current Inventory:",'magenta', attrs = ['underline']))
 			inventory.show()
-			print("\nWhat would you like to do?\nEnter add, show, or quit.")
-			user_in = input('-->')
+			
+		elif user_in == 'size':
+			print(colored("Inventory Size:", 'magenta', attrs = ['underline']))
+			inventory.size()
+		
+		elif user_in == 'q':
+			print ("Peace out, you benevolent kitten cuddler.")
+			return 
 		
 		else:
-			print("Follow directions. Who hired you?")
-			print("What would you like to do?\nEnter add, show, or quit.")
-			user_in = input('-->')
-		
-	return ("Peace out, you benevolent kitten cuddler.")
-	
+			print(colored("Invalid input.\n",'red'))
+			
 if __name__ == '__main__':
     main()
